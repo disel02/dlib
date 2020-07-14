eb9afc43426e601105413f0cf16b4747fbcd5711
disel02 <diselkamble9@gmail.com>
2020-07-14 17:02:45 +0530

    1.2

--- dlibsoft/src/main/java/com/tzutalin/dlib/FaceRec.java
+++ dlibsoft/src/main/java/com/tzutalin/dlib/FaceRec.java
@@ -1,14 +1,15 @@
 package com.tzutalin.dlib;
 
 import android.graphics.Bitmap;
-import android.support.annotation.Keep;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
-import android.support.annotation.WorkerThread;
 import android.util.Log;
 
 import java.util.Arrays;
 import java.util.List;
+
+import androidx.annotation.Keep;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.annotation.WorkerThread;
 
 /**
  * Created by houzhi on 16-10-20.
